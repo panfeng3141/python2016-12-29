d0dbc93ec5e45855531ffe8a3dfeebecbbae3eae
panfeng3141 <1131308065@qq.com>
2016-12-29 20:14:29 +0800

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,27 @@
+# -- coding: utf-8 -- 
+#转义字符
+print "I am 6'2\" tall."
+print 'I am 6\'2" tall.'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#更多转义字符
+#\\ 反斜杠符号
+#\' 单引号
+#\" 双引号
+#\t 横向指标符
+#\r 回车
